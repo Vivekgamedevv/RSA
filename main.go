2de07ca50b123282d7d0dc0be2c500a7bd4cf984
Vivekgamedev <67198204+Vivekgamedevv@users.noreply.github.com>
2025-02-06 09:37:40 +0530

    Update main.go
    in this update we ask the user for the response so we can decrypt the encrypted file and mainly pem file is being generated in this update

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"crypto/rand"
 	"crypto/rsa"
 	"crypto/sha256"
+	"crypto/x509"
+	"encoding/pem"
 	"fmt"
 	"io"
 	"os"
@@ -40,44 +42,15 @@ func (eg *licensefile) open_file() {
 		fmt.Println("Error in opening the file")
 	}
 
-	file.WriteString("Expiry_date:")
+	file.WriteString("expiry_date:")
 	file.WriteString(eg.expiry_date)
 	file.WriteString("\n")
 	file.WriteString("User_id:")
 	file.WriteString(eg.user_id)
 }
 
-// function to decrypt the encrypted file
-func decryptfile(encryptedfile, outputfile string, Sprivatekey *rsa.PrivateKey) {
-	file, err := os.Open(encryptedfile)
-	if err != nil {
-		fmt.Println("Error in opening the encrypted file")
-	}
-
-	ciphertext, err := io.ReadAll(file)
-	if err != nil {
-		fmt.Println("Error in reading the encrypted file")
-	}
-
-	abc := rand.Reader
-	label := []byte("")
-	plaintext, _ := rsa.DecryptOAEP(sha256.New(), abc, Sprivatekey, ciphertext, label)
-
-	decryptedfile, err := os.Create(outputfile)
-	if err != nil {
-		fmt.Println("Error in creating the decrypted file")
-	}
-
-	_, err = decryptedfile.Write(plaintext)
-
-	if err != nil {
-		fmt.Println("Error in writing the decrypted text to the outputfile file")
-	}
-
-}
-
 // funtion to encrypt the raw file
-func encryptfile(inputfile string, outputfile string, Sprivatekey *rsa.PrivateKey) {
+func encryptfile(inputfile string, outputfile string, Sprivatekey *rsa.PrivateKey) error {
 
 	file, err := os.Open(inputfile)
 	if err != nil {
@@ -107,12 +80,44 @@ func encryptfile(inputfile string, outputfile string, Sprivatekey *rsa.PrivateKe
 		fmt.Println("Error in writing the cipher text into the outputfile")
 	}
 
-	decryptfile(Encryptedfile, "decryptedfile.txt", Sprivatekey)
+	//decryptfile(Encryptedfile, "decryptedfile.txt", Sprivatekey)
+
+	return nil
 
 }
 
+// function to decrypt the encrypted file
+func decryptfile(encryptedfile, outputfile string, Sprivatekey *rsa.PrivateKey) error {
+	file, err := os.Open(encryptedfile)
+	if err != nil {
+		fmt.Println("Error in opening the encrypted file")
+	}
+
+	ciphertext, err := io.ReadAll(file)
+	if err != nil {
+		fmt.Println("Error in reading the encrypted file")
+	}
+
+	abc := rand.Reader
+	label := []byte("")
+	plaintext, _ := rsa.DecryptOAEP(sha256.New(), abc, Sprivatekey, ciphertext, label)
+
+	decryptedfile, err := os.Create(outputfile)
+	if err != nil {
+		fmt.Println("Error in creating the decrypted file")
+	}
+
+	_, err = decryptedfile.Write(plaintext)
+
+	if err != nil {
+		fmt.Println("Error in writing the decrypted text to the outputfile file")
+	}
+
+	return nil
+}
+
 // Function used to generate system private and public key
-func GeneratePrivatekeyFile() {
+func GeneratePrivatekeyFile() *rsa.PrivateKey {
 
 	Sprivatekey, err := rsa.GenerateKey(rand.Reader, 2048)
 
@@ -121,9 +126,95 @@ func GeneratePrivatekeyFile() {
 
 	}
 
-	encryptfile(filename, "Encryptedfile.txt", Sprivatekey)
+	c := Sprivatekey
+	privKeyBytes := x509.MarshalPKCS1PrivateKey(Sprivatekey)
 
+	// Create a PEM block
+	privPemBlock := &pem.Block{
+		Type:  "RSA PRIVATE KEY",
+		Bytes: privKeyBytes,
+	}
+
+	// Create or overwrite the file
+	file, err := os.Create("privatekey.pem")
+	if err != nil {
+		fmt.Println("Failed to create a privatekey file")
+	}
+	defer file.Close()
+
+	// Encode the PEM block to the file
+	err = pem.Encode(file, privPemBlock)
+	if err != nil {
+		fmt.Println("Failed to encode the pem block to the file")
+	}
+
+	//encryptfile(filename, "Encryptedfile.txt", Sprivatekey)
+	return c
 }
+
+// // validation of license AK
+// func validateLicense(filePath string) (bool, error) {
+// 	file, err := os.Open(filePath)
+// 	if err != nil {
+// 		return false, fmt.Errorf("error opening the file: %w", err)
+// 	}
+// 	defer file.Close()
+
+// 	content, err := io.ReadAll(file)
+// 	if err != nil {
+// 		return false, fmt.Errorf("error reading the file: %w", err)
+// 	}
+
+// 	lines := strings.Split(string(content), "\n")
+// 	for _, line := range lines {
+// 		if strings.HasPrefix(line, "expiry_date:") {
+// 			dateStr := strings.TrimSpace(strings.TrimPrefix(line, "expiry_date:"))
+// 			expiryDate, err := time.Parse("2006-01-02", dateStr)
+// 			if err != nil {
+// 				return false, fmt.Errorf("invalid date format in the file: %w", err)
+// 			}
+
+// 			if time.Now().Before(expiryDate) {
+// 				return true, nil
+// 			}
+// 			return false, nil
+// 		}
+// 	}
+// 	return false, fmt.Errorf("expiry_date not found in the file")
+// }
+
+// checing if the License file exists or no
+
+// func checkFileExists(filePath string) bool {
+// 	_, err := os.Stat(filePath)
+// 	if os.IsNotExist(err) {
+// 		return false
+// 	}
+// 	return err == nil
+// }
+
+// If the validation is true now the contents in the file will be shown
+
+// func displaycontentinfile(filepath string) error {
+
+// 	file, err := os.Open(filepath)
+// 	if err != nil {
+// 		return fmt.Errorf("error while opening the file !! : %w", err)
+// 	}
+// 	defer file.Close()
+
+// 	content, err := io.ReadAll(file)
+// 	if err != nil {
+// 		return fmt.Errorf("error while reading the file !! : %w ", err)
+
+// 	}
+
+// 	fmt.Println("Input given by user :")
+
+// 	fmt.Println(string(content))
+// 	return nil
+
+// }
 
 func main() {
 	lcfile := licensefile{}
@@ -131,5 +222,69 @@ func main() {
 	lcfile.read_info(&temp1, &temp2)
 	fmt.Printf("The expiry date of the license file is %s \n", temp1)
 	lcfile.open_file()
-	GeneratePrivatekeyFile()
+	prkey := GeneratePrivatekeyFile()
+	encryptfile(filename, "Encryptedfile.txt", prkey)
+
+	err := encryptfile(filename, "Encryptedfile.txt", prkey)
+
+	if err != nil {
+		fmt.Println("Error in encrypting the file")
+	}
+
+	fmt.Println("File encrypted succesfully")
+
+	fmt.Println(prkey)
+
+	var condi string
+	fmt.Println("Do you want to decrypt the file(Y/N):")
+	fmt.Scanln(&condi)
+	if condi == "Y" {
+
+		inputkey := new(rsa.PrivateKey)
+		fmt.Println("Input the private key to decrypt:")
+
+		err = decryptfile(Encryptedfile, "decryptedfile.txt", inputkey)
+		if err != nil {
+			fmt.Println("Error in decrypting the file")
+		}
+
+		fmt.Println("File decrypted succesfully")
+
+	} else if condi == "N" {
+		fmt.Println("File decryption stopped")
+	} else {
+		fmt.Println("Wrong input")
+	}
+
+	// // validation of license
+	// valid, err := validateLicense(filename)
+	// if err != nil {
+	// 	fmt.Println("Error validating the license:", err)
+	// 	return
+	// }
+
+	// // to display the contents present inside the file
+
+	// if valid {
+	// 	fmt.Println("------------------------------------------")
+	// 	fmt.Println(" License is valid. Access granted.\n", "displaying the contents inside the file:")
+	// 	fmt.Println("-----------------------------------------")
+	// 	err := displaycontentinfile(filename)
+	// 	if err != nil {
+	// 		fmt.Println("error while displaying the file : %w", err)
+	// 	}
+	// } else {
+	// 	fmt.Println("License has expired. Access denied !!! .")
+	// }
+
+	// // file checking for every 24 hours
+	// if checkFileExists(filename) {
+	// 	fmt.Println("File exists.")
+	// } else {
+
+	// 	fmt.Println("File does not exist.")
+	// }
+
+	// time.Sleep(10 * time.Second)
+
 }
